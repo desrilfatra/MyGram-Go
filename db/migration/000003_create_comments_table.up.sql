e17609981af0583be3d46a43910a49207e33ceee
desrilfatra <desrilfatra8@gmail.com>
2022-06-25 07:05:56 +0700

    db migration comments

---
+++ db/migration/000003_create_comments_table.up.sql
@@ -0,0 +1,12 @@
+begin;
+
+create table if not exists comment(
+Id SERIAL primary key,
+User_id int not null references public.users(id),
+Photo_id int not null references public.photo(id),
+Caption varchar(100) not null,
+CreatedAt date not null,
+UpdatedAt date not null
+);
+
+commit;
