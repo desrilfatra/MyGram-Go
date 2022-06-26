229dd1ffe7baf545e53cd0d8d687fe10d134abf3
desrilfatra <desrilfatra8@gmail.com>
2022-06-27 00:23:47 +0700

    message

--- db/migration/000003_create_comments_table.up.sql
+++ db/migration/000003_create_comments_table.up.sql
@@ -4,7 +4,7 @@ create table if not exists comment(
 Id SERIAL primary key,
 User_id int not null references public.users(id),
 Photo_id int not null references public.photo(id),
-Caption varchar(100) not null,
+Message varchar(100) not null,
 CreatedAt date not null,
 UpdatedAt date not null
 );
